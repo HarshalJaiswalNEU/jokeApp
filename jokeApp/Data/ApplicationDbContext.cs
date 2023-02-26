4555c538f49556bea90710954ec17352c567b05a
HarshalJaiswalNEU <jaiswal.ha@northeastern.edu>
2023-02-25 20:41:50 -0500

    initial commit

---
+++ jokeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jokeApp.Models;
+
+namespace jokeApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<jokeApp.Models.Joke> Joke { get; set; } = default!;
+}
+
---
+++ jokeApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace jokeApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
